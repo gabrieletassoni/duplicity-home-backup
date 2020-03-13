a648d43df0d019caba3071b7d76ae3df09b1cfdd
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2020-03-13 12:20:45 +0100

    add auto creation of remote backup dir

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 Adjust in bin/backup-config.sh the remote.
 Copy the content of *crontab-e* inside ```crontab -e``` editor.
 Copy *bin* content into bin folder under your home.
+
+# SSH for Rsync
+
+ssh-keygen -t rsa
+chmod 700 ~/.ssh && chmod 600 ~/.ssh/*
+ssh-copy-id -i ~/.ssh/id_rsa.pub -p 22 rsync@192.168.121.105
+ssh -p '22' 'rsync@192.168.121.105'
--- bin/backup-config.sh
+++ bin/backup-config.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -e
 
-REMOTE="rsync://rsync@192.168.121.11//volume1/NetBackup/$USER"
+RSYNCUSER="rsync"
+RSYNCURL="192.168.121.105"
+RSYNCCONNECTION="$RSYNCUSER@$RSYNCURL"
+RSYNCDIR="/volume1/NetBackup/$USER"
+
+ssh $RSYNCCONNECTION "mkdir -p $RSYNCDIR"
+
+REMOTE="rsync://$RSYNCCONNECTION/$RSYNCDIR"
 EXCLUDES="--exclude-filelist $HOME/bin/homedir-excludes.conf"
 INCLUDES=""
